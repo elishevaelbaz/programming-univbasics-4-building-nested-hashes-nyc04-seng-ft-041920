f6f41359b2524779026fa54ffa4273c02716cddf
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-23 15:08:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -68,10 +68,12 @@ def monopoly_with_fourth_tier
           {
             :mortgage_value => 200
           },
-          :b_and_o_railroad => {
+          :b_and_o_railroad => 
+          {
             :mortgage_value => 400
           },
-          :shortline_railroad => {
+          :shortline_railroad => 
+          {
             :mortgage_value => 800
           },
         }
